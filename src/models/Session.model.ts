dc9a09c6c2f703b92ef7aee85a0e8dc03c9f1f66
emreuzelgok <emreuzelgok@gmail.com>
2021-11-21 01:00:42 +0300

    Added Session model

---
+++ src/models/Session.model.ts
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { IUserDocument } from "./User.model";
+import { Schema, Document, model } from "mongoose";
+
+export interface ISessionDocument extends  Document {
+  user: IUserDocument["_id"];
+  valid: boolean;
+  userAgent: string;
+  ip: string;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+const SessionSchema: Schema<ISessionDocument> = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  valid: { type: Boolean, default: true },
+  userAgent: { type: String },
+  ip: { type: String }
+}, {
+  timestamps: true
+})
+
+export default model<ISessionDocument>('Session', SessionSchema);
