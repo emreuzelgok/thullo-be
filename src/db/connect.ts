3c9070cb75dbe7e246b7dbb9da5d07e9fa18abfd
emreuzelgok <emreuzelgok@gmail.com>
2021-11-21 00:25:49 +0300

    Initial commit

---
+++ src/db/connect.ts
@@ -0,0 +1,14 @@
+import { connect } from "mongoose";
+import log from "../logger";
+
+const connectDB = async () => {
+  try {
+    await connect(process.env.MONGODB_URI);
+    log.info(`Connected Database: ${process.env.MONGODB_URI}`);
+  } catch (error) {
+    log.error(`Database connectioin error: ${error}`);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import connectDB from "./db/connect";
+import log from "./logger";
+
+require('express-async-errors');
+require('dotenv').config();
+
+const app: Application = express();
+const port: number = parseInt(process.env.PORT);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use(cors({ origin: [`http://localhost:${port}`] }));
+
+app.get('/', (_req, res) => {
+  res.send('Thullo API 1.0');
+});
+
+app.listen(port, () => {
+  log.info(`server is listening on ${port}`);
+  connectDB();
+});
+ 
