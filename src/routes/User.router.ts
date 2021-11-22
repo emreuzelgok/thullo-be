11a1b8820a3c20e13fe4a79ee53446b10823edb6
emreuzelgok <emreuzelgok@gmail.com>
2021-11-22 18:00:48 +0300

    Added user schema on swagger

--- src/routes/User.router.ts
+++ src/routes/User.router.ts
@@ -5,6 +5,45 @@ import { createUserSchema } from "../schemas/User.schema";
 
 const UserRouter = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *          description: Auto generated user id
+ *        firstName:
+ *          type: string
+ *          description: The User firstname
+ *          min: 3
+ *          max: 50
+ *        lastName:
+ *          type: string
+ *          description: The User lastname
+ *          min: 3
+ *          max: 50
+ *        email:
+ *          type: string
+ *          description: The User email
+ *        password:
+ *          type: string
+ *          description: The User password
+ *        avatar:
+ *          type: string
+ *          description: The User avatar path
+ *        bio:
+ *          type: string
+ *          description: The User biography
+ *      required: 
+ *        - firstName
+ *        - lastName
+ *        - email
+ *        - password
+ *       
+ */
 UserRouter.post('/', validateRequest(createUserSchema), createUserHandler);
 
 export default UserRouter;
